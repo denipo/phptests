b09c10470b71385a7bd8da9fc9318b882a398aa7
denipo <denys.popov@gmail.com>
2013-08-01 16:18:16 +0300

    first version

--- index.php
+++ index.php
@@ -6,3 +6,9 @@ echo 'test ';
 $book['title']= 'how to stop worrying and love atomic bomb';
 
 echo $book['title'];
+
+$play = 'Shakespere\'s "Macbeth" ';
+$ground = "Sakespere's \"Macbeth\" ";
+
+echo $play;
+echo $ground;
