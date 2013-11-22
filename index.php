09e0aa0feb70d80c8dde9c7b808cb7feda8f6944
denipo <denys.popov@gmail.com>
2013-11-22 02:04:55 +0200

    first version

--- index.php
+++ index.php
@@ -3,12 +3,74 @@
 echo 'test ';
 
 
-$book['title']= 'how to stop worrying and love atomic bomb';
+$book['title']= 'how to stop worrying and love atomic bomb  ';
 
 echo $book['title'];
+echo "<br>";
 
-$play = 'Shakespere\'s "Macbeth" ';
+
+$play = ' Shakespere\'s "Macbeth" ';
 $ground = "Sakespere's \"Macbeth\" ";
+echo "<br>";
 
 echo $play;
 echo $ground;
+echo "<br>";
+
+echo date("Y");
+echo date("M");
+echo ("_______");
+echo phpversion();
+echo "<br>";
+
+/* concatenation
+echo $play.$ground;
+*/
+
+$name = 'John';
+$lastName = 'Dove';
+
+
+echo $name.' '.$lastName;
+echo "<br>";
+
+$fruit = '2 apples';
+$veg = '2 carrots';
+echo $fruit+$veg;
+echo "<br>";
+
+$fruit = '2 apples';
+$veg = 'and 2 carrots';
+echo $fruit+$veg;
+echo "<br>";
+
+echo $fruit;
+echo "<br>";
+
+
+$fruit.=' Yohoho добавил строку ';
+echo "<br>";
+
+echo $fruit;
+echo "<br>";
+echo "<br>";
+
+echo "math";
+echo "<br>";
+
+$x = 20;
+$y = 10;
+$z = 3;
+
+echo 26%5;
+echo 26%27;
+echo 10%2;
+echo "<br>";
+
+$now = new DateTime();
+echo $now;
+
+
+
+
+
